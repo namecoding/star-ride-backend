ceb926691d6591c7d4146d6bfb682d1e1f1d3020
namecoding <namecodingteam@gmail.com>
2026-04-21 22:41:46 +0100

    imported notificationController wrongly again

--- src/routes/notificationRoutes.js
+++ src/routes/notificationRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
+import { protect } from "../middlewares/authMiddleware.js";
 import {
     getNotifications
   } from "../controllers/notificationController.js";
 
     const router = express.Router();
     
-    router.get("/get-notifications", getNotifications);
+    router.get("/get", protect, getNotifications);
 
 
 
